ad40488a76fa88b622504519c53315b0b6a22aac
Dušan Uverić <dusan.uveric9@gmail.com>
2020-04-23 15:19:54 +0200

    Implementacija dekoratora za funkcije
    Ovi dekoratori služe da izbegnemo kod koji se ponavlja
    
    Signed-off-by: Dušan Uverić <dusan.uveric9@gmail.com>

---
+++ src/dekoratori/dekoratori.py
@@ -0,0 +1,27 @@
+def isgreaterthan(argument):
+    def main_wrapper(function):
+        def wrapper(*args):
+            if args[1] <= argument:
+                raise ValueError('Neispravna vrednost')
+            return function(*args)
+        return wrapper
+    return main_wrapper
+
+def wrapisinstance(argument):
+    def main_wrapper(function):
+        def wrapper(*args):
+            if isinstance(args[1], argument):
+                return function(*args)
+            else:
+                raise TypeError(f'Nepravilan tip argumenta: {type(args[1])}, ocekuje se {argument}')
+        return wrapper
+    return main_wrapper
+
+def isinrange(argument):
+    def main_wrapper(function):
+        def wrapper(*args):
+            if not(argument[0] <= args[1] <= argument[1]):
+                raise ValueError('Neispravna vrednost')
+            return function(*args)
+        return wrapper
+    return main_wrapper
