6dbf6b5c91aa59d91089c6a4aa3a0ae5285cb700
Brs <jancicboris56@gmail.com>
2020-04-24 14:24:56 +0200

    Promene nad klasom Delovi
    Signed-off-by: Dušan Uverić <dusan.uveric9@gmail.com>

--- src/modeli/delovi.py
+++ src/modeli/delovi.py
@@ -4,49 +4,55 @@ class Delovi(Identifikacija):
     def __init__(self, oznaka: str, naziv: str, duzina: float, sirina: float, visina: float, \
                         tezina : float, opis: str, broj: int, pod_delovi: int, materijal: str ):
         super().__init__(oznaka)
-        self._naziv = naziv
-        self._duzina = duzina
-        self._sirina = sirina
-        self._visina = visina
-        self._tezina = tezina
-        self._opis = opis
-        self._broj = broj
-        self._pod_delovi = pod_delovi
-        self._materijal = materijal
-    
+        self.naziv = naziv
+        self.duzina = duzina
+        self.sirina = sirina
+        self.visina = visina
+        self.tezina = tezina
+        self.opis = opis
+        self.broj = broj
+        self.pod_delovi = pod_delovi
+        self.materijal = materijal
+
+    def __str__(self):
+        return "{0}, {1}, {2}, {3}, {4}, {5}, {6}, {7}, {8}, {9}".format(self._oznaka, \
+                self._naziv, self._duzina, self._sirina, self._visina, self._tezina,
+                self._opis, self._broj, self._pod_delovi, self._materijal)
+
     @property
     def naziv(self):
         return self._naziv
 
     @naziv.setter
     def naziv(self, naziv):
-        if (naziv != ""):
-            self._naziv = naziv
-        else:
+        if not naziv:
             raise ValueError("Ime ne sme biti prazan tekst !")
-    
+        self._naziv = naziv
+
+
+
     @property
     def duzina(self):
         return self._duzina
     
     @duzina.setter
     def duzina(self, duzina):
-        if (duzina > 0):
-            self._duzina = duzina
-        else:
+        if (duzina < 0):
             raise ValueError("Ne sme biti dodeljena vrednost manja ili jednaka 0 !")
+        self._duzina = duzina
 
-    
+
+
     @property
     def sirina(self):
         return self._sirina
 
     @sirina.setter
     def sirina(self, sirina):
-        if (sirina > 0):
-            self._sirina
-        else:
+        if (sirina < 0):
             raise ValueError("Ne sme biti dodeljena vrednost manja ili jednaka 0 !")
+        self._sirina = sirina
+
 
 
     @property
@@ -55,10 +61,10 @@ class Delovi(Identifikacija):
     
     @visina.setter
     def visina(self, visina):
-        if (visina > 0):
-            self._visina
-        else:
+        if (visina < 0):
             raise ValueError("Ne sme biti dodeljena vrednost manja ili jednaka 0 !")
+        self._visina = visina
+
 
 
     @property
@@ -67,10 +73,11 @@ class Delovi(Identifikacija):
     
     @tezina.setter
     def tezina(self, tezina):
-        if (tezina > 0):
-            self._tezina
-        else:
+        if (tezina < 0):
             raise ValueError("Ne sme biti dodeljena vrednost manja ili jednaka 0 !")
+        self._tezina = tezina
+
+
 
     @property
     def opis(self):
@@ -78,9 +85,10 @@ class Delovi(Identifikacija):
     
     @opis.setter
     def opis(self, opis):
-        self._opis
+        self._opis = opis
 
-    
+
+
     @property
     def broj(self):
         return self._broj
@@ -90,32 +98,25 @@ class Delovi(Identifikacija):
         self._broj = broj
 
 
+
     @property
     def pod_delovi(self):
         return self._pod_delovi
 
     @pod_delovi.setter
     def pod_delovi(self, pod_delovi):
-        if (pod_delovi > 0 and pod_delovi is type(int)):
-            self._pod_delovi = pod_delovi 
-        else:
-            raise ValueError("Broj pod-delova ne sme biti manja od nule i mora biti CELOBROJAN broj")
+        if (pod_delovi <= 0):
+            raise ValueError("Broj pod-delova ne sme biti manja od nule !")
+        self._pod_delovi = pod_delovi
 
-    
+
+
     @property
     def materijal(self):
         return self._materijal
     
     @materijal.setter
     def materijal(self, materijal):
-        if (materijal != ""):
-            self._materijal = materijal
-        else:
+        if not materijal:
             raise ValueError("Materijal ne sme biti prazan tekst !")
-
-
-    def __str__(self):
-        return "{0}, {1}, {2}, {3}, {4}, {5}, {6}, {7}, {8}, {9}".format(self._oznaka, \
-                self._naziv, self._duzina, self._sirina, self._visina, self._tezina, \
-                self._opis, self._broj, self._pod_delovi, self._materijal)
-                
+        self._materijal = materijal
