b990e6fdc9b842902685039085f658f11c62ee8b
Dušan Uverić <dusan.uveric9@gmail.com>
2020-04-26 20:06:54 +0200

    Implementacija pomoćne funkcije za čišćenje terminala/konzole

---
+++ src/helper/shell.py
@@ -0,0 +1,15 @@
+from os import system, name
+
+
+# shell_clear function
+# taken from stackoverflow
+
+
+def shell_clear():
+    # for windows
+    if name == 'nt':
+        _ = system('cls')
+
+    # for mac and linux(here, os.name is 'posix')
+    else:
+        _ = system('clear')
