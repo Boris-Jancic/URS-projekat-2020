ea73f1a727e3740f03750568c7300216274ae28b
Brs <jancicboris56@gmail.com>
2020-05-08 21:48:45 +0100

    Dodati dekoratori

--- src/modeli/deo.py
+++ src/modeli/deo.py
@@ -1,8 +1,10 @@
+from dekoratori.dekoratori import isgreaterthan, wrapisinstance, isinrange, isnotempty
 from modeli.identifikacija import Identifikacija
+
 
 class Deo(Identifikacija):
     def __init__(self, oznaka: str, naziv: str, duzina: float, sirina: float, visina: float, \
-                        tezina : float, opis: str, broj: int, pod_delovi: int, materijal: str ):
+                 tezina: float, opis: str, broj: int, pod_delovi: int, materijal: str):
         super().__init__(oznaka)
         self.naziv = naziv
         self.duzina = duzina
@@ -16,107 +18,88 @@ class Deo(Identifikacija):
 
     def __str__(self):
         return "{0}, {1}, {2}, {3}, {4}, {5}, {6}, {7}, {8}, {9}".format(self._oznaka, \
-                self._naziv, self._duzina, self._sirina, self._visina, self._tezina,
-                self._opis, self._broj, self._pod_delovi, self._materijal)
+                                                                         self._naziv, self._duzina, self._sirina,
+                                                                         self._visina, self._tezina,
+                                                                         self._opis, self._broj, self._pod_delovi,
+                                                                         self._materijal)
 
     @property
     def naziv(self):
         return self._naziv
 
     @naziv.setter
+    @wrapisinstance(str)
     def naziv(self, naziv):
-        if not naziv:
-            raise ValueError("Ime ne sme biti prazan tekst !")
         self._naziv = naziv
-
-
 
     @property
     def duzina(self):
         return self._duzina
-    
+
     @duzina.setter
+    @wrapisinstance((float, int))
+    @isgreaterthan(0)
     def duzina(self, duzina):
-        if (duzina < 0):
-            raise ValueError("Ne sme biti dodeljena vrednost manja ili jednaka 0 !")
         self._duzina = duzina
-
-
 
     @property
     def sirina(self):
         return self._sirina
 
     @sirina.setter
+    @isgreaterthan(0)
     def sirina(self, sirina):
-        if (sirina < 0):
-            raise ValueError("Ne sme biti dodeljena vrednost manja ili jednaka 0 !")
         self._sirina = sirina
-
-
 
     @property
     def visina(self):
         return self._visina
-    
+
     @visina.setter
+    @isgreaterthan(0)
     def visina(self, visina):
-        if (visina < 0):
-            raise ValueError("Ne sme biti dodeljena vrednost manja ili jednaka 0 !")
         self._visina = visina
-
-
 
     @property
     def tezina(self):
         return self._tezina
-    
+
     @tezina.setter
+    @isgreaterthan(0)
     def tezina(self, tezina):
-        if (tezina < 0):
-            raise ValueError("Ne sme biti dodeljena vrednost manja ili jednaka 0 !")
         self._tezina = tezina
-
-
 
     @property
     def opis(self):
         return self._opis
-    
+
     @opis.setter
+    @isnotempty
     def opis(self, opis):
         self._opis = opis
-
-
 
     @property
     def broj(self):
         return self._broj
-        
+
     @broj.setter
     def broj(self, broj):
         self._broj = broj
-
-
 
     @property
     def pod_delovi(self):
         return self._pod_delovi
 
     @pod_delovi.setter
+    @isgreaterthan(-1)
     def pod_delovi(self, pod_delovi):
-        if (pod_delovi <= 0):
-            raise ValueError("Broj pod-delova ne sme biti manja od nule !")
         self._pod_delovi = pod_delovi
-
-
 
     @property
     def materijal(self):
         return self._materijal
-    
+
     @materijal.setter
+    @isnotempty
     def materijal(self, materijal):
-        if not materijal:
-            raise ValueError("Materijal ne sme biti prazan tekst !")
         self._materijal = materijal
--- src/modeli/kamion.py
+++ src/modeli/kamion.py
@@ -1,7 +1,9 @@
+from dekoratori.dekoratori import isgreaterthan, wrapisinstance, isinrange, isnotempty
 from modeli.identifikacija import Identifikacija
 
+
 class Kamion(Identifikacija):
-    def __init__(self,oznaka: str, model: str, max_tezina: float, broj_automobila: int):
+    def __init__(self, oznaka: str, model: str, max_tezina: float, broj_automobila: int):
         super().__init__(oznaka)
         self.model = model
         self.max_tezina = max_tezina
@@ -12,35 +14,29 @@ class Kamion(Identifikacija):
         return self._model
 
     @model.setter
+    @wrapisinstance(model, str)
+    @isnotempty
     def model(self, model):
-        if not model:
-            raise ValueError("Zadata vrednost je prazan")
         self._model = model
-
-
 
     @property
     def max_tezina(self):
         return self._max_tezina
 
     @max_tezina.setter
+    @wrapisinstance(int)
+    @isgreaterthan(0)
     def max_tezina(self, max_tezina):
-        if(isinstance(max_tezina , int) or max_tezina < 0):
-            raise ValueError("Zadata vrednost je manja od nule ili nije decimalna vrednost")
         self._max_tezina = max_tezina
-
-
 
     @property
     def broj_automobila(self):
         return self._broj_automobila
 
     @broj_automobila.setter
+    @isinrange((1, 7))
     def broj_automobila(self, broj_automobila):
-        if(broj_automobila <= 1 or broj_automobila >= 6):
-            raise ValueError("Kamion moze podrzati 1-6 automobila")
         self._broj_automobila = broj_automobila
 
-    
     def __str__(self):
         return "{0}, {1}, {2}, {3}".format(self._oznaka, self._model, self._max_tezina, self._broj_automobila)
