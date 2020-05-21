ad5661cf011203c9c4753287faf860dfe00ce397
Boris Jancic <jancicboris56@gmail.com>
2020-05-20 23:16:24 -0700

    Dodate validacije za kamion i deo
    Signed-off-by: Dušan Uverić <dusan.uveric9@gmail.com>

--- src/helper/oznaka_validator.py
+++ src/helper/oznaka_validator.py
@@ -1,4 +1,6 @@
 from io_loader.automobil_io import AutomobilLoader
+from io_loader.deo_io import DeoLoader
+from io_loader.kamion_io import KamioniLoader
 
 
 class OznakaError(Exception):
@@ -7,5 +9,17 @@ class OznakaError(Exception):
 
 def validacija_oznake_za_automobil(oznaka: str):
     for _ in AutomobilLoader.get_loaded_cars():
+        if _.oznaka == oznaka:
+            raise OznakaError("Neispravna oznaka!")
+
+
+def validacija_oznake_za_deo(oznaka: str):
+    for _ in DeoLoader.get_loaded_parts():
+        if _.oznaka == oznaka:
+            raise OznakaError("Neispravna oznaka!")
+
+
+def validacija_oznake_za_kamion(oznaka: str):
+    for _ in KamioniLoader.get_loaded_trucks():
         if _.oznaka == oznaka:
             raise OznakaError("Neispravna oznaka!")
