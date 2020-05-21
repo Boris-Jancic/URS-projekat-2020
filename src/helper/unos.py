b34f3db4004ca18a811e360b3ce54f682fcb8e3d
Boris Jancic <jancicboris56@gmail.com>
2020-05-20 23:16:59 -0700

    Dodat upis za kamione i delove
    Signed-off-by: Dušan Uverić <dusan.uveric9@gmail.com>

--- src/helper/unos.py
+++ src/helper/unos.py
@@ -1,6 +1,8 @@
 from io_writer import automobil_io
 from io_writer import radnik_io 
 from io_writer import odsek_io
+from io_writer import deo_io
+from io_writer import kamioni_io
 
 def unos() -> str:
     return input("Izaberite jednu od opcija: ")
@@ -8,5 +10,7 @@ def unos() -> str:
 
 def upis_u_fajl():
     automobil_io.dump()
+    kamioni_io.dump()
+    deo_io.dump()
     radnik_io.upis()
     odsek_io.upis()
